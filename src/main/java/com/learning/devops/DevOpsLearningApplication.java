3a1f4b5a921044f71999e87a9d4aa664b0465fb7
Varun Kumar <varun.kumar12aug@gmail.com>
2025-08-09 15:45:39 +0530

    Added first time

---
+++ src/main/java/com/learning/devops/DevOpsLearningApplication.java
@@ -0,0 +1,13 @@
+package com.learning.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/devops/DevOpsLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
