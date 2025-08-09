616126c2e59006f9d7ddca88cf1a297ddad8a0e0
Varun Kumar <varun.kumar12aug@gmail.com>
2025-08-09 22:16:24 +0530

    added

--- src/main/java/com/learning/LearningApplication.java
+++ src/main/java/com/learning/LearningApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LearningApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("aws.java.v1.disableDeprecationAnnouncement", "true");
 		SpringApplication.run(LearningApplication.class, args);
 	}
 
